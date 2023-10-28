4fdaf12acdf92e1505c4de99910e36eee2349a89
Jin-Tankuji <92167075+Jin-Tankuji@users.noreply.github.com>
2023-10-28 10:07:48 +0000

    Open main.py to read code

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ X = data.data
 y = data.target
 
 # Chia dữ liệu thành tập huấn luyện và tập kiểm tra
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    test_size=0.2,
+                                                    random_state=42)
 
 # Khởi tạo một mô hình SVM
 model = SVC(kernel='linear', C=1.0)
@@ -28,8 +31,10 @@ print(f'Accuracy: {accuracy}')
 from sklearn.model_selection import GridSearchCV
 
 # Thiết lập các giá trị tham số bạn muốn thử nghiệm
-param_grid = {'C': [0.1, 1, 10],
-              'kernel': ['linear', 'rbf', 'poly', 'sigmoid']}
+param_grid = {
+    'C': [0.1, 1, 10],
+    'kernel': ['linear', 'rbf', 'poly', 'sigmoid']
+}
 
 # Sử dụng Grid Search để tìm giá trị tốt nhất cho các tham số
 grid_search = GridSearchCV(SVC(), param_grid, cv=5)
@@ -38,3 +43,10 @@ grid_search.fit(X_train, y_train)
 # In ra các giá trị tốt nhất
 print(f'Best parameters: {grid_search.best_params_}')
 print(f'Best accuracy: {grid_search.best_score_}')
+
+# Đánh giá mô hình tốt nhất trên tập kiểm tra
+best_model = grid_search.best_estimator_
+y_test_pred = best_model.predict(X_test)
+
+test_accuracy = accuracy_score(y_test, y_test_pred)
+print(f'Accuracy on test set using the best model: {test_accuracy}')
