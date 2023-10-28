da87bcc5e6c849c914944bf7ae308cf056686c67
Jin <92167075+Jin-Tankuji@users.noreply.github.com>
2023-10-28 17:15:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,7 @@ X = data.data
 y = data.target
 
 # Chia dữ liệu thành tập huấn luyện và tập kiểm tra
-X_train, X_test, y_train, y_test = train_test_split(X,
-                                                    y,
-                                                    test_size=0.2,
-                                                    random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Khởi tạo một mô hình SVM
 model = SVC(kernel='linear', C=1.0)
